508aa39e1120189bbac3eb26e45da1cf34e912a6
Kosorotikov-AO@home <darkringfire@gmail.com>
2018-03-31 01:47:24 +0800

    refactory. RESET-PRESENCE

--- ow-gpio/main.cpp
+++ ow-gpio/main.cpp
@@ -6,30 +6,37 @@
  */ 
 #define F_CPU 16000000
 
+#define T_RESET 118
+#define T_RELAX 4
+#define T_PRESENCE 30
+
 #define T_PRESCALE_1    0b001<<CS00;
 #define T_PRESCALE_8    0b010<<CS00;
 #define T_PRESCALE_64   0b011<<CS00;
 
-#define W1_WAIT_RISE() { EICRA = 0b11 << ISC00; EIFR = 1<<INTF0; EIMSK = 1<<INT0; }
-#define W1_WAIT_FALL() { EICRA = 0b10 << ISC00; EIFR = 1<<INTF0; EIMSK = 1<<INT0; }
-#define W1_NOWAIT() { EIMSK = 0; }
-#define T0_OFF() { TIMSK0 = 0;  TCCR0B = 0;  }
-#define T0_15US() { TIFR0 = 1<<TOV0; TIMSK0 = 1<<TOIE0; TCNT0 = 0xff - 240; GTCCR = 1<<PSRSYNC; TCCR0B = T_PRESCALE_1; }
-#define T0_120US() { TIFR0 = 1<<TOV0; TIMSK0 = 1<<TOIE0; TCNT0 = 0xff - 240; GTCCR = 1<<PSRSYNC; TCCR0B = T_PRESCALE_8; }
-#define T0_480US() { TIFR0 = 1<<TOV0; TIMSK0 = 1<<TOIE0; TCNT0 = 0xff - 118; GTCCR = 1<<PSRSYNC; TCCR0B = T_PRESCALE_64; }
+//#define W1_WAIT_RISE { EICRA = 0b11 << ISC00; EIFR = 1<<INTF0; EIMSK = 1<<INT0; }
+//#define W1_WAIT_FALL { EICRA = 0b10 << ISC00; EIFR = 1<<INTF0; EIMSK = 1<<INT0; }
+#define W1_WAIT_CHANGE { EICRA = 0b01 << ISC00; }
+//#define W1_NOWAIT { EIMSK = 0; }
+//#define T0_OFF { TIMSK0 = 0;  TCCR0B = 0;  }
+//#define T0_15US { TIFR0 = 1<<TOV0; TIMSK0 = 1<<TOIE0; TCNT0 = 0xff - 240; GTCCR = 1<<PSRSYNC; TCCR0B = T_PRESCALE_1; }
+//#define T0_120US { TIFR0 = 1<<TOV0; TIMSK0 = 1<<TOIE0; TCNT0 = 0xff - 240; GTCCR = 1<<PSRSYNC; TCCR0B = T_PRESCALE_8; }
+//#define T0_480US { TIFR0 = 1<<TOV0; TIMSK0 = 1<<TOIE0; TCNT0 = 0xff - 118; GTCCR = 1<<PSRSYNC; TCCR0B = T_PRESCALE_64; }
+#define T0_RUN(t) { GTCCR = 1<<PSRSYNC; TCNT0 = 0; OCR0B = t; TCCR0B = T_PRESCALE_64; }
+#define T0_STOP { TCCR0B = 0; }
 
-#define LED_ON() { PORTB |= 1<<5; }
-#define LED_OFF() { PORTB &= ~1<<5; }
-#define LED_IMP() { LED_ON(); LED_OFF(); }
+#define LED_ON { PORTB |= 1<<5; }
+#define LED_OFF { PORTB &= ~1<<5; }
+#define LED_IMP { LED_ON; LED_OFF; }
     
-#define W1_SEND_0() { DDRD |= 1<<2; }
-#define W1_SEND_1() { DDRD &= ~1<<2; }
+#define W1_SEND_0 { DDRD |= 1<<2; }
+#define W1_SEND_1 { DDRD &= ~1<<2; }
 
-#define W1_STATE_WAIT_RESET		    0
+#define W1_STATE_IDLE		    0
 #define W1_STATE_RESET              1
 #define W1_STATE_WAIT_RESET_END     2
 #define W1_STATE_WAIT_SEND_PRESENCE 3
-#define W1_STATE_SEND_PRESENCE      4
+#define W1_STATE_PRESENCE      4
 #define W1_STATE_WAIT_COMMAND       5
 
 #include <avr/io.h>
@@ -44,16 +51,19 @@ inline void init()
 	//debug
 	DDRB = 1<<5;
 	
-	w1_state = W1_STATE_WAIT_RESET;
+	w1_state = W1_STATE_IDLE;
 	
 	// INT0
-	W1_WAIT_FALL();
+	W1_WAIT_CHANGE;
 	EIMSK = 1 << INT0;
 	
 	//TC0
 	//Mode: normal
 	TCCR0A = 0<<WGM00;
 	TCCR0B = 0<<WGM02;
+    TIMSK0 = 1<<OCIE0A | 1<<OCIE0B;
+    OCR0A = T_RESET;
+    OCR0B = 0;
 	
 	sei();
 }
@@ -61,56 +71,52 @@ inline void init()
 
 ISR(INT0_vect)
 {
-	switch(w1_state)
-	{
-        // wait FALL
-		case W1_STATE_WAIT_RESET:
-			W1_WAIT_RISE();
-			T0_480US();
-            w1_state = W1_STATE_RESET;
-			break;
-        // wait RISE
-        case W1_STATE_WAIT_RESET_END:
-            W1_NOWAIT();
-            T0_15US();
-            w1_state = W1_STATE_WAIT_SEND_PRESENCE;
-            break;
-        case W1_STATE_RESET:
-            W1_WAIT_FALL();
-            T0_OFF();
-            w1_state = W1_STATE_WAIT_RESET;
-            break;
-		default:
-			W1_WAIT_FALL();
-			w1_state = W1_STATE_WAIT_RESET;
+    if (PIND & 1<<PIND2) {
+        // was RISE
+        switch(w1_state) {
+            case W1_STATE_RESET:
+                T0_RUN(T_RELAX);
+                break;
+            default:
+                T0_STOP;
+        }
+    } else {
+        // was FALL
+        switch(w1_state) {
+            case W1_STATE_RESET:
+                W1_SEND_0;
+                w1_state = W1_STATE_PRESENCE;
+                T0_RUN(T_PRESENCE);
+                break;
+            case W1_STATE_PRESENCE:
+                break;
+            default:
+                T0_RUN(0);
+        }
 	}
 }
 
-ISR(TIMER0_OVF_vect)
+ISR(TIMER0_COMPA_vect)
 {
-	T0_OFF();
-    switch(w1_state)
-    {
+    T0_STOP;
+    w1_state = W1_STATE_RESET;
+}
+
+ISR(TIMER0_COMPB_vect)
+{
+    switch(w1_state) {
         case W1_STATE_RESET:
-            w1_state = W1_STATE_WAIT_RESET_END;
+            W1_SEND_0;
             break;
-        case W1_STATE_WAIT_SEND_PRESENCE:
-            T0_120US();
-            w1_state = W1_STATE_SEND_PRESENCE;
-            W1_SEND_0();
-            // TODO
-            LED_ON();
+        case W1_STATE_PRESENCE:
+            W1_SEND_1;
+            T0_STOP;
+            //TODO
+            w1_state = W1_STATE_IDLE;
             break;
-        case W1_STATE_SEND_PRESENCE:
-            w1_state = W1_STATE_WAIT_COMMAND;
-            W1_WAIT_FALL();
-            W1_SEND_1();
-            // TODO
-            LED_OFF();
-            w1_state = W1_STATE_WAIT_RESET;
-            break;
-            
-    }            
+        default:
+            ;
+    }
 }
 
 int main(void)
